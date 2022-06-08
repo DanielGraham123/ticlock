15a48884076e8e9ab9b27f2ec2eba33d8bd136bf
DanielGraham123 <dg.danielboaz@yahoo.com>
2022-06-09 00:39:53 +0100

    added some styling to app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import logo from "./assets/Ticlock.png";
 
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center flex-col text-center bg-[#050A30]">
-      <div className="">
+    <div className="w-full h-screen overflow-y-hidden flex justify-center flex-col items-center bg-[#050A30]">
+      <div className="mt-[-60px]">
         <img
           src={logo}
           alt="logo image"
@@ -14,20 +14,20 @@ function App() {
         />
       </div>
 
-      <h1 className="py-8 text-lg text-gray-500">Hello There !</h1>
+      <h1 className="pt-6 text-lg text-gray-500">Hello There!</h1>
 
-      <div className=" pb-2 px-4">
-        <small className="text-teal-700">Today is:</small>
-        <h1 className="text-2xl md:text-3xl duration-300 text-gray-500">
-          {moment(new Date().toLocaleDateString()).format("dddd, MMMM Do YYYY")}
-        </h1>
-      </div>
-
-      <div className="pt-6 pb-8 px-4">
+      <div className="py-8 px-4 text-center">
         <small className="text-red-800">The time is:</small>
         <h3 className="text-4xl md:text-6xl font-medium duration-300 text-gray-500">
           {new Date().toLocaleTimeString()}
         </h3>
+      </div>
+
+      <div className=" pb-2 px-4 text-center">
+        <small className="text-teal-700">Today is:</small>
+        <h1 className="text-xl md:text-3xl duration-300 text-gray-500">
+          {moment(new Date().toLocaleDateString()).format("dddd, MMMM Do YYYY")}
+        </h1>
       </div>
     </div>
   );
